c5ea488d2d68c2b0a3438982e04591175957b66e
finefine <tianrandailoving@gmail.com>
2021-09-27 15:18:07 +0800

    feat: add pkg/app

---
+++ pkg/app/flag.go
@@ -0,0 +1,25 @@
+// Copyright 2021 Ke Fan <litesky@foxmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package app
+
+import (
+	"strings"
+
+	"github.com/spf13/pflag"
+)
+
+//nolint: deadcode,unused,varcheck
+func initFlag() {
+	pflag.CommandLine.SetNormalizeFunc(WordSepNormalizeFunc)
+}
+
+// WordSepNormalizeFunc changes all flags that contain "_" separators.
+func WordSepNormalizeFunc(_ *pflag.FlagSet, name string) pflag.NormalizedName {
+	if strings.Contains(name, "_") {
+		return pflag.NormalizedName(strings.Replace(name, "_", "-", -1))
+	}
+
+	return pflag.NormalizedName(name)
+}
